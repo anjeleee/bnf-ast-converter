b50496348789e5f93daf14ea5f58c494ddb6cb74
anjelee <198414525+anjeleee@users.noreply.github.com>
2026-09-05 16:43:53 +0800

    Fix configurations

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     port: 5173,
     proxy: {
-      '/parse': 'https://bnf-ast-converter.onrender.com/health',
+      '/parse': 'https://bnf-ast-converter.onrender.com/parse',
       '/health': 'https://bnf-ast-converter.onrender.com/health'
     }
   }
